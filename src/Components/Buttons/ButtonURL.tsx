ff00e3866ee62b05cf90512249c4daf784edcf21
jammy312 <james.brutus@polymtl.ca>
2024-05-31 11:29:37 -0400

    update position and add image

--- src/Components/Buttons/ButtonURL.tsx
+++ src/Components/Buttons/ButtonURL.tsx
@@ -2,15 +2,17 @@ import viteLogo from '/vite.svg'
 import '../../styles/ButtonURL.css'
 interface Props {
     url: string;
+    imagePath?:string;
   }
 
 
-const ButtonURL: React.FC<Props> = ({ url }) => {
+const ButtonURL: React.FC<Props> = ({ url,imagePath }) => {
+    const path:string = imagePath ?? viteLogo;
 
     return (
         <div>
             <a href= {url} target="_blank">
-            <img src={viteLogo}  className='size' alt="Vite logo" />
+            <img src={path}  className='size' alt="Vite logo" />
             </a>
         </div>
 
