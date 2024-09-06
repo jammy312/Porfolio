5d9602a14f1f6621c3613ddeb4ac053bdea3cf8d
jammy312 <james.brutus@polymtl.ca>
2024-09-06 15:13:43 -0400

    local storage

--- src/Components/Buttons/LanguageButton.tsx
+++ src/Components/Buttons/LanguageButton.tsx
@@ -1,21 +1,35 @@
-import { ChangeEvent, useState } from "react";
-import i18n from "../../i18n";
+import { useState, useEffect, ChangeEvent } from "react";
+import i18n from "i18next";
 import CheckBoxButton from "./CheckBoxButton";
 
 function LanguageButton() {
     const [isChecked, setIsChecked] = useState(false);
-   
-    const handleCheckboxChange = (event:ChangeEvent<HTMLInputElement>) => {
-        setIsChecked(event.target.checked);
-        if(isChecked){
-            i18n.changeLanguage("en");
-        }else {
-            i18n.changeLanguage("fr");
+
+    useEffect(() => {
+        const storedLanguage = localStorage.getItem("language");
+        if (storedLanguage) {
+            setIsChecked(storedLanguage === "fr"); 
         }
-      };
-    return (<>
-        <CheckBoxButton leftButtonName="Fr" rightButtonName="En" onChange={handleCheckboxChange} checked={isChecked}/>
-    </>);
+    }, []);
+
+    const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const newIsChecked = event.target.checked;
+        setIsChecked(newIsChecked);
+        const newLanguage = newIsChecked ? "fr" : "en";
+        i18n.changeLanguage(newLanguage);
+        localStorage.setItem("language", newLanguage); 
+    };
+
+    return (
+        <>
+            <CheckBoxButton 
+                leftButtonName="Fr" 
+                rightButtonName="En" 
+                onChange={handleCheckboxChange} 
+                checked={isChecked} 
+            />
+        </>
+    );
 }
 
 export default LanguageButton;
--- src/i18n.tsx
+++ src/i18n.tsx
@@ -17,7 +17,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en', 
+    lng: localStorage.getItem("language") ?? "en" , 
     fallbackLng: 'en', 
 
     interpolation: {
